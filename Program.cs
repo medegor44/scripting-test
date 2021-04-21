863b39c97926840c80db87a11fba798de7fa12b3
medegor44 <medegor44@gmail.com>
2021-04-21 17:02:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+
+namespace scripting_test
+{
+    public class Globals
+    {
+        public Func<string, string> Print = (str) =>
+        {
+            Console.WriteLine(str);
+            return str + "$";
+        };
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var sr = new StreamReader("input.txt"))
+            {
+                var script = sr.ReadToEnd();
+
+                var res = await CSharpScript.RunAsync(script, globals: new Globals());
+            }
+        }
+    }
+}
