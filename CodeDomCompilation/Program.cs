bcc93d5e03f1ab6d4faa808e691faccc0b4d9391
medegor44 <medegor44@gmail.com>
2021-05-11 18:34:20 +0300

    Add CodeDom compilation

---
+++ CodeDomCompilation/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Linq;
+
+namespace CodeDomCompilation
+{
+    class Program
+    {
+        static void Main()
+        {
+            var provider = new CSharpCodeProvider();
+            var param = new CompilerParameters(new[] { typeof(object).Assembly.Location }, "a.exe", true);
+
+            using var sr = new StreamReader("input.txt");
+            var code = sr.ReadToEnd();
+
+            var res = provider.CompileAssemblyFromSource(param, code);
+
+            res.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText));
+        }
+    }
+}
