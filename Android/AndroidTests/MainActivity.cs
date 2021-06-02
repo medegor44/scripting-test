a6838ea73e04149521c6bb5470e41692cf1bf01d
medegor44 <medegor44@gmail.com>
2021-06-02 10:26:48 +0300

    Add android unti test project

---
+++ Android/AndroidTests/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Android.App;
+using Android.OS;
+using Xamarin.Android.NUnitLite;
+
+namespace AndroidTests
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+    public class MainActivity : TestSuiteActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            // tests can be inside the main assembly
+            this.AddTest(Assembly.GetExecutingAssembly());
+            // or in any reference assemblies
+            // AddTest (typeof (Your.Library.TestClass).Assembly);
+
+            // Once you called base.OnCreate(), you cannot add more assemblies.
+            base.OnCreate(bundle);
+        }
+    }
+}
---
+++ Android/AndroidTests/TestSample.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ScriptingLibrary;
+
+namespace AndroidTests
+{
+    [TestFixture]
+    public class TestSample
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // write your test fixture setup
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // write your test fixture teardown 
+        }
+
+        [Test]
+        public async Task Pass()
+        {
+            var runner = new ScriptCodeRunner("var a = 1;", new Container());
+            await runner.RunAsync();
+        }
+
+        [Test]
+        public async Task ResolveFromContainer_Succeed()
+        {
+            var cnt = 0;
+            Action f = () => cnt++;
+
+            var container = new Container();
+            container.Register(f, "f");
+
+            var code = "f();";
+
+            var runner = new ScriptCodeRunner(code, container);
+            await runner.RunAsync();
+
+            Assert.AreEqual(1, cnt);
+        }
+    }
+}
