3a5005a247f627066464964577eb04090601496c
Egor Medvedev <Egor.Medvedev@confirmit.com>
2021-06-29 16:49:25 +0300

    Add LENS code runner

---
+++ Shared/ScriptingLibrary/LensCodeRunner.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Lens;
+
+namespace ScriptingLibrary
+{
+    public class LensCodeRunner : ICodeRunner
+    {
+        public LensCodeRunner(string code, Container container)
+        {
+            Code = code;
+            Container = container;
+        }
+
+        public async Task RunAsync()
+        {
+            var compiler = new LensCompiler();
+
+            foreach (var entry in Container.Names)
+            {
+                var name = entry.Value;
+                var type = entry.Key;
+
+                compiler.RegisterProperty(name, () => Container.Resolve(type));
+            } 
+            var compiled = compiler.Compile(Code);
+            await Task.Run(compiled);
+        }
+
+        private string Code { get; set; }
+        private Container Container { get; set; }
+    }
+}
---
+++ Shared/ScriptingLibraryTests/LensCodeRunnerTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using HelperObjects;
+using ScriptingLibrary;
+using Xunit;
+
+namespace ScriptingLibraryTests
+{
+    public class LensCodeRunnerTests
+    {
+        [Fact]
+        public async void Run()
+        {
+            var code = "let a = 1";
+            var runner = new LensCodeRunner(code, new Container());
+            await runner.RunAsync();
+        }
+
+        [Fact]
+        public async void RunWithGlobalVariables_Succeed()
+        {
+            var code = @"
+let a = 1 
+
+let b = a + t.x
+
+if b <> 2 then
+    throw new Exception";
+
+            var obj = new HelperObject { x = 1 };
+
+            var container = new Container();
+            container.Register(obj, "t");
+
+            var runner = new LensCodeRunner(code, container);
+
+            await runner.RunAsync();
+        }
+
+        [Fact]
+        public async void RunWithPrimitiveGlobalVariables_Succeed()
+        {
+            var code = @"
+let a = 1 
+let b = a + t
+if b <> 2 then
+    print 1";
+
+            var container = new Container();
+            container.Register(1, "t");
+
+            var runner = new LensCodeRunner(code, container);
+
+            await runner.RunAsync();
+        }
+
+        [Fact]
+        public async void ExternalFuncWillCall()
+        {
+            var code = "f 2";
+            int cnt = 0;
+
+            var container = new Container();
+            container.Register<Action<int>>((x) =>
+            {
+                cnt += x;
+            }, "f");
+
+            var runner = new LensCodeRunner(code, container);
+
+            await runner.RunAsync();
+
+            Assert.Equal(2, cnt);
+        }
+    }
+}
